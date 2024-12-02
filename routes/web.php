2f30af68aa2349d50fc0de36f85515ea99fd0543
idn-fahmifn <fahmifn.idn@gmail.com>
2024-12-02 10:56:00 +0700

    parameter

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route Sederhana.
+
+/**
+ * Route Sederhana
+ * method get
+ */
 Route::get('fahmi', function(){
     return 'Ini adalah respon fahmi'; //ini adalah respon yang akan ditampilkan di web.
 });
@@ -18,5 +22,11 @@ Route::get('halaman-fahmi', function(){
 Route::get('data-fahmi', function(){
     $nama = 'Fahmi Nuradi'; //data
     return $nama; //callback untuk memanggil data
+});
+
+// Route dengan parameter
+// digunakan untuk membuat halaman edit atau halaman detail.
+Route::get('halaman/{parameter}', function($data){
+    return 'ini adalah halaman : '.$data;
 });
 
