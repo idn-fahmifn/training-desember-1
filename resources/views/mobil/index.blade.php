acf91fcf82057c711e2160e3bcc8ba82ec013ca2
idn-fahmifn <fahmifn.idn@gmail.com>
2024-12-03 16:56:54 +0700

    template index

--- resources/views/mobil/index.blade.php
+++ resources/views/mobil/index.blade.php
@@ -1,32 +1,42 @@
 @extends('template.template')
 
 @section('content')
-<table class="table table-striped">
-    <!-- header -->
-    <thead>
-        <th>Nama Mobil</th>
-        <th>No Polisi</th>
-        <th>Pilihan</th>
-    </thead>
 
-    <!-- data mobil -->
-    <tbody>
-        <!-- looping data -->
-        @foreach ($data as $item)
-        <tr>
-            <!-- data yang di looping masuk ke dalaman tag TD -->
-            <td>{{$item->nama_mobil}}</td>
-            <td>{{$item->no_polisi}}</td>
-            <td>
-                <form action="{{route('mobil.delete', $item->id)}}" method="post">
-                    @csrf
-                    {{method_field('delete')}}
-                    <a href="{{route('mobil.detail', $item->id)}}">detail</a>
-                    <button type="submit" onclick="return confirm('Yakin mau dihapus?')">Hapus</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="card mt-4">
+    <div class="card-body">
+        <h4 class="card-title">Halaman Data Mobil</h4>
+
+        <table class="table table-striped mt-4">
+            <!-- header -->
+            <thead>
+                <th>Nama Mobil</th>
+                <th>No Polisi</th>
+                <th>Pilihan</th>
+            </thead>
+
+            <!-- data mobil -->
+            <tbody>
+                <!-- looping data -->
+                @foreach ($data as $item)
+                <tr>
+                    <!-- data yang di looping masuk ke dalaman tag TD -->
+                    <td>{{$item->nama_mobil}}</td>
+                    <td>{{$item->no_polisi}}</td>
+                    <td>
+                        <form action="{{route('mobil.delete', $item->id)}}" method="post">
+                            @csrf
+                            {{method_field('delete')}}
+                            <a class="btn btn-info" href="{{route('mobil.detail', $item->id)}}">detail</a>
+                            <button class="btn btn-danger" type="submit" onclick="return confirm('Yakin mau dihapus?')">Hapus</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
+</div>
+
+
 @endsection
